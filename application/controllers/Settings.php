64937fd661f7a42f3fcc8f2e2f725d1e3bd5f3af
gihantha <gihantha01@gmail.com>
2024-06-13 12:46:41 +0530

    Created a Page for user profile with controller

---
+++ application/controllers/Settings.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Settings extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('user');
+	}
+
+    function user_profile(){
+        $this->load->view('user_profile');
+    }
+}
---
+++ application/views/user_profile.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php $this->load->view("includes/head") ?>
+</head>
+
+<body id="page-top">
+    <?php $this->load->view("includes/navbar") ?>
+
+    <!-- services Grid-->
+    <section class="page-section bg-light" id="services">
+        <div class="container-fluid align-items-center justify-content-center">
+
+
+
+            <!-- Page Heading -->
+            <center>
+                <h1 class="h3 mb-2  text-gray-800 justify-content:center">User Profile</h1>
+            </center>
+            
+
+                    <div class="container-fluid justify-content:center">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="card mb-4">
+                                    <div class="card-header">
+                                        User Details
+                                    </div>
+                                    <div class="card-body">
+                                        <!-- <form class="user"> -->
+                                        <div class="form-group ">
+
+                                            <label>User Name</label>
+                                            <input type="text" class="form-control form-control-user" id="name" placeholder="Please Enter Name"  disabled>
+
+
+                                        </div>
+                                        <div class="form-group row">
+
+                                            <div class="col-sm-6 mb-3 mb-sm-0">
+                                                <label>User Phone</label>
+
+                                                <input type="text" class="form-control form-control-user" id="phone" placeholder="Please Enter Phone Number" >
+                                            </div>
+
+                                        </div>
+                                        <div class="form-group">
+                                            <label>User Email</label>
+
+                                            <input type="email" class="form-control form-control-user" id="exampleInputEmail" placeholder="Email Address" disabled>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>User Address</label>
+
+                                            <textarea class="form-control form-control-user" placeholder="Address" id="address">
+                                            
+                                        </textarea>
+
+                                        </div>
+                                        <!-- <div class="form-group">
+                                        <label>User Password</label>
+
+                                        <input type="text" class="form-control form-control-user" id="exampleInputEmail" placeholder="User Password">
+                                    </div> -->
+
+                                        <button class="btn btn-primary btn-user btn-block" >
+                                            Update Profile
+                                        </button>
+                                        <hr>
+
+                                        <!-- </form> -->
+                                    </div>
+
+                                </div>
+
+                            </div>
+                            <div class="col-md-6">
+
+                                <div class="card mb-4">
+                                    <div class="card-header">
+                                        <center><img width="150px" class="img-profile rounded-circle" src="<?php echo base_url() ?>assets/assets/img/undraw_profile.svg"></center>
+                                    </div>
+                                    <div class="card-body">
+                                        <h4><b>Admin</b></h4>
+                                        <p>Engineer</p>
+                                    </div>
+                                </div>
+                                <div class="card mb-4">
+                                    <div class="card-header">
+                                        <h3>Password Reset</h3>
+                                    </div>
+                                    <div class="card-body">
+                                        <form class="user" id="changeps">
+                                            <div class="form-group ">
+                                                <label>Old Password</label>
+                                                <input type="text" class="form-control form-control-user" placeholder="Please Enter Old Password" id="oldpass" required>
+                                            </div>
+                                            <div class="form-group ">
+                                                <label>New Password</label>
+                                                <input type="text" class="form-control form-control-user" placeholder="Please Enter New Password" id="newpass" required>
+                                            </div>
+                                            <div class="form-group ">
+                                                <label>Confirm Password</label>
+                                                <input type="text" class="form-control form-control-user" placeholder="Please Enter New Password" id="conpass" required>
+                                            </div>
+                                            <button type="submit" class="btn btn-primary btn-user btn-block"> Change Password </button>
+                                        </form>
+                                    </div>
+                                </div>
+
+
+                            </div>
+
+                        </div>
+
+
+
+
+
+
+                    </div>
+        </div>
+    </section>
+
+
+    <?php $this->load->view("includes/footer") ?>
+    <?php $this->load->view("includes/js") ?>
+</body>
+
+
+</html>
