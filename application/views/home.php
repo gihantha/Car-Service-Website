37008ccd41347a4e0a8b87dc82960600444b6ef8
gihantha <gihantha01@gmail.com>
2024-06-13 12:23:53 +0530

    Creating Home Page

---
+++ application/views/home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<?php $this->load->view("includes/head") ?>
+</head>
+
+<body id="page-top">
+<?php $this->load->view("includes/navbar") ?>
+	
+
+<?php $this->load->view("includes/footer") ?>
+	<?php $this->load->view("includes/js") ?>
+</body>
+
+
+</html>
